f19f699409cc61367bf56f285cfe11c74b7f9931
Amy Kleynhans <amykleynhans@yahoo.com>
2023-10-11 19:02:35 -0600

    added put route to update posts

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,19 +1,53 @@
 const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
 
-    const response = await fetch(`/api/blogs/${id}`, {
-      method: 'DELETE',
-    });
+        const response = await fetch(`/api/blogs/${id}`, {
+            method: 'DELETE',
+        });
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Failed to delete post');
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert('Failed to delete post');
+        }
     }
-  }
+};
+
+const updateButtonHandler = async (event) => {
+
+    const name = document.querySelector('#newName').value.trim();
+    const content = document.querySelector('#newContent').value.trim();
+
+
+    if (event.target.hasAttribute('data-id')) {
+
+        if (name && content) {
+
+            const id = event.target.getAttribute('data-id');
+
+            const response = await fetch(`/api/blogs/${id}`, {
+                method: 'PUT',
+                body: JSON.stringify({ name, content }),
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+            });
+
+            if (response.ok) {
+                document.location.replace(`/blog/${id}`);
+            } else {
+                alert('Failed to update post');
+            }
+
+        }
+    }
 };
 
 document
-  .querySelector('.blog-list')
-  .addEventListener('click', delButtonHandler);
+    .querySelector('.blog-list')
+    .addEventListener('click', delButtonHandler);
+document
+    .querySelector('.update')
+    .addEventListener('click', updateButtonHandler);
+
